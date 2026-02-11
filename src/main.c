4d8e8e54461240767ec4ef610ca64c2f418fd0f4
Daniel <daniel101shi@gmail.com>
2026-02-11 15:23:44 -0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
   fgets(COMMAND, sizeof(COMMAND), stdin);
-  COMMAND[strcspn[COMMAND, '\n']] = '\0';
+  COMMAND[strcspn(COMMAND, '\n')] = '\0';
 
   printf("%s: command not found", COMMAND);
   
